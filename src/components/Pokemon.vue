ec7694fc02c79e2132f8d11eb69de5011a4966bb
avanvalk <drewvv@gmail.com>
2018-11-27 14:53:44 -0800

    added css

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <li>
+    <img v-bind:src="pokemon.img" />
+    <div class="info">
+      <h3>{{ pokemon.name }}</h3>
+      <p>attack: {{ pokemon.attack }}</p>
+      <p>defense: {{ pokemon.defense }}</p>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemon: Object
+  }
+};
+</script>
+
+<style scoped>
+li {
+  position: relative;
+  height: 300px;
+}
+img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  object-fit: cover;
+}
+.info {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background: rgba(255, 255, 255, 0.8);
+  text-align: center;
+}
+h3, p {
+  margin: 0;
+}
+</style>

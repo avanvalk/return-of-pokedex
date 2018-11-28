256ca32614984865c085c08b512a9053a37bbbfb
avanvalk <drewvv@gmail.com>
2018-11-27 21:18:41 -0800

    fixed the name filter!

--- src/App.vue
+++ src/App.vue
@@ -40,8 +40,8 @@ export default {
   computed: {
     filtered() {
       return this.pokemons.filter(pokemon => {
-        const hasName = !this.filter.name || pokemon.pokemon >= this.filter.name;
-        const hasType = !this.filter.type_1 || pokemon.type_1 === this.filter.type_1;
+        const hasName = !this.filter.name || pokemon.pokemon.includes(this.filter.name);
+        const hasType = !this.filter.type_1 || pokemon.type_1.includes(this.filter.type_1);
         const hasAttack = !this.filter.attack || pokemon.attack >= this.filter.attack;
         const hasDefense = !this.filter.defense || pokemon.defense >= this.filter.defense;
         return hasName && hasType && hasAttack && hasDefense;

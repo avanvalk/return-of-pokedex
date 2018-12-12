66837a857075fea1cb1fdd9ff8f464749870d513
avanvalk <drewvv@gmail.com>
2018-12-12 14:21:40 -0800

    started on staggered transition

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,5 @@
 <template>
+
   <header>
 
     <label for="name"> Name:
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,6 +1,14 @@
 
 <template>
-  <li>
+<transition-group 
+  name="staggered-transition"
+  v-on:before-enter="beforeEnter"
+  v-on:enter="enter"
+  v-on:leave="leave"
+>
+  <li
+  v-bind:key="pokemon"
+  >
     <img v-bind:src="pokemon.url_image"/>
     <div class="info">
       <h3>{{ pokemon.pokemon }}</h3>
@@ -9,14 +17,45 @@
       <p>health: {{ pokemon.hp }}</p>
     </div>
   </li>
+</transition-group>
 </template>
 
 <script>
+
 export default {
   props: {
     pokemon: Object
+  },
+  methods: {
+    beforeEnter: function(el) {
+      el.style.opacity = 0;
+      el.style.height = 0;
+    },
+    enter: function(el, done) {
+      var delay = el.dataset.index * 150;
+      setTimeout(function() {
+        Velocity(
+          el,
+          { opacity: 1, height: '1.6em' },
+          { complete: done }
+        );
+      }, delay);
+    },
+    leave: function(el, done) {
+      var delay = el.dataset.index * 150;
+      setTimeout(function() {
+        Velocity(
+          el,
+          { opacity: 0, height: 0 },
+          { complete: done }
+        );
+      }, delay);
+    }
   }
 };
+
+
+
 </script>
 
 <style scoped>
